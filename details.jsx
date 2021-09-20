a90a019b69aa683eadab0042c3f6854609f10a92
hisreal123 <omosgoerge@gmail.com>
2021-09-20 13:25:41 +0100

    first-commit

---
+++ details.jsx
@@ -0,0 +1,8 @@
+// 1 // Creating Netflix frontpage first 
+    // create a firbase projects and link up 
+        // npm install -g firebase-tools
+        // Deploy hosting 
+    // Create-react-app using snowpack
+        // install sass
+            // npm i -g  @snowpack/plugins
+        // install react-router-dom
---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.sass'
+
+
+function App()
+{
+  return (
+    <div>
+      <p>Working </p>
+    </div>
+  )
+}
+
+export default App
